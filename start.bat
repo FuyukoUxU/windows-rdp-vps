3e34f6d55375df5281cce2aaf0f77078584bb32f
Shirakami Yuu <85018619+yuu-chan-baka@users.noreply.github.com>
2022-02-17 17:18:42 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By Aiko" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By Aiko( Yuu stolen roi nha)" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 net user administrator @aiko2k6 /add >nul
